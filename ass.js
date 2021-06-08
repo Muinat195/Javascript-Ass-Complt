7e6729b2741689429c547fd30a8f00e7048c4588
Muinah <74907083+Muinat195@users.noreply.github.com>
2021-06-08 09:19:53 +0100

    Add files via upload

---
+++ ass.js
@@ -0,0 +1,45 @@
+//functions problem set
+
+//loops problem set 
+//using for
+number = [51];
+for (var i = 0; i < number; i = i + 2) {
+    console.log(i);
+}
+// //using while
+var i = 2;
+while ( i<51) {
+    console.log(i);
+   i-i+2;
+}
+//fizzbuzz
+var number =[101];
+for(var i = 1; i < number; i++){
+if (i%3==0){
+console.log("fizz");}
+else if(i%5==0){
+    console.log("Buzz");
+}
+else {
+    console.log(i);
+}
+}
+//iseven
+let number = prompt("Enter any value");
+function IsEven(value){
+   value = prompt("enter any value");
+       if (number%2 ==1){
+return("False");
+}
+else{
+   return("True");
+}
+}
+//SCII ART
+var asc = "$";
+for(var i = "$"; i <= "$$$$$$$"; i = i + "$")
+{
+    console.log(i);
+
+}
+
